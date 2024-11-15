fcacdf342ac665d3229c5e4d9bbb0573ee3960ff
irfnhanif <14.irfanh@gmail.com>
2024-11-15 10:24:49 +0700

    Implement stock data fetching and processing in fetch handler

--- src/index.js
+++ src/index.js
@@ -8,8 +8,22 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import 'dotenv/config';
+import { processStock } from './functions/process-stock.js';
+
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const { stocks } = await request.json();
+		const apiKey = process.env.STOCK_API_KEY;
+
+		const stockResponses = await Promise.all(stocks.map((symbol) => processStock(symbol, apiKey)));
+		const result = stockResponses.reduce((acc, { symbol, data }) => {
+			if (data) acc[symbol] = data;
+			return acc;
+		}, {});
+
+		return new Response(JSON.stringify(result), {
+			headers: { 'Content-Type': 'application/json' },
+		});
 	},
 };
