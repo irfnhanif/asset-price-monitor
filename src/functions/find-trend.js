f501b8c8f0c1bcc0a41562e2bae29af496d03976
irfnhanif <14.irfanh@gmail.com>
2024-11-15 10:05:36 +0700

    Refactor trend determination functions and rename file for better organization

--- src/functions/find-trend.js
+++ src/functions/find-trend.js
@@ -1,13 +1,12 @@
-const calculateMovingAverage = (data, period = 100) => {
-	const timeSeries = data['Time Series (Daily)'];
+function calculateMovingAverage(timeSeries, period = 100) {
 	const dates = Object.keys(timeSeries).sort((a, b) => new Date(b) - new Date(a));
-
 	const closePrices = dates.slice(0, period).map((date) => parseFloat(timeSeries[date]['4. close']));
 	return closePrices.reduce((sum, price) => sum + price, 0) / closePrices.length;
-};
+}
 
-export const determineTrend = (data) => {
-	const latestClosePrice = parseFloat(data['Time Series (Daily)'][Object.keys(data['Time Series (Daily)'])[0]]['4. close']);
-	const ma100 = calculateMovingAverage(data);
+export function determineTrend(timeSeries) {
+	const dates = Object.keys(timeSeries).sort((a, b) => new Date(b) - new Date(a));
+	const latestClosePrice = parseFloat(timeSeries[dates[0]]['4. close']);
+	const ma100 = calculateMovingAverage(timeSeries);
 	return latestClosePrice > ma100 ? 'Uptrend' : 'Downtrend';
-};
+}
