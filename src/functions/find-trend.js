b5099e26bd923339335185cd8ef5b5ec6fc2f994
irfnhanif <14.irfanh@gmail.com>
2024-11-24 17:58:56 +0700

    Enhance moving average calculation and trend determination to support multiple data types

--- src/functions/find-trend.js
+++ src/functions/find-trend.js
@@ -1,12 +1,14 @@
-function calculateMovingAverage(timeSeries, period = 100) {
-	const dates = Object.keys(timeSeries).sort((a, b) => new Date(b) - new Date(a));
-	const closePrices = dates.slice(0, period).map((date) => parseFloat(timeSeries[date]['4. close']));
+function calculateMovingAverage(timeSeries, period = 100, type) {
+	const dates = Object.keys(timeSeries).sort((a, b) => (type === 'stock' ? new Date(b) - new Date(a) : new Date(a) - new Date(b)));
+	const closePrices = dates
+		.slice(0, period)
+		.map((date) => parseFloat(type === 'stock' ? timeSeries[date]['4. close'] : timeSeries[date][1]));
 	return closePrices.reduce((sum, price) => sum + price, 0) / closePrices.length;
 }
 
-export function determineTrend(timeSeries) {
-	const dates = Object.keys(timeSeries).sort((a, b) => new Date(b) - new Date(a));
-	const latestClosePrice = parseFloat(timeSeries[dates[0]]['4. close']);
-	const ma100 = calculateMovingAverage(timeSeries);
+export function determineTrend(timeSeries, type) {
+	const dates = Object.keys(timeSeries).sort((a, b) => (type === 'stock' ? new Date(b) - new Date(a) : new Date(a) - new Date(b)));
+	const latestClosePrice = parseFloat(type === 'stock' ? timeSeries[dates[0]]['4. close'] : timeSeries[dates[0]][1]);
+	const ma100 = calculateMovingAverage(timeSeries, 100, type);
 	return latestClosePrice > ma100 ? 'Uptrend' : 'Downtrend';
 }
